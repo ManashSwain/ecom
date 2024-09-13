cbb7913714d51e15c2c55f6eb64d537ef228ba26
ManashSwain <manash0802@gmail.com>
2024-09-14 01:03:13 +0530

    added pincode check feature

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -6,6 +6,24 @@ import { useRouter } from 'next/router';
 const Slug = () => {
     const router = useRouter();
     const {slug} = router.query ;
+    
+    const [pin , setPin] = useState( );
+    const [pinService , setpinService] = useState();
+
+    const checkPincode = async()=>{
+      let pinvalue = await fetch("http://localhost:3000/api/pincode");
+      let pincheck = await pinvalue.json();
+      if(pincheck.includes(parseInt(pin))){
+         setpinService(true);
+      }
+      else{
+        setpinService(false);
+      }
+    }
+
+    const handleChange = (e)=>{
+      setPin(e.target.value);
+    }
     
   return (
     <>
@@ -88,14 +106,22 @@ const Slug = () => {
           </div>
         </div>
         <div className="flex">
-          <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
-          <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
-          <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-            <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-            </svg>
-          </button>
+          <span className="title-font mx-4 font-medium text-2xl text-gray-900">$58.00</span>
+
+          <div className='flex'>
+          <button className="flex ml-auto mx-4 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
+          <button className="flex ml-auto mx-4 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded">Buy Now</button>
+          </div>  
         </div>
+        <div className="pincode  my-5 p-3 flex">
+            <input className='p-1 border-2 rounded-md border-blue-500 outline-none' type="text" onChange={handleChange}  placeholder='Enter your pincode' />
+            <button  onClick={checkPincode}  className=" flex  mx-4 text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Check</button>
+          </div>
+          <div>
+            {(!pinService && pinService != null  ) && <p className='text-red-600'>Sorry! We do not deliver to this pincode</p>}
+            {(pinService && pinService != null  ) && <p className='text-green-600'>yay! This pincode is serviceable</p>}
+            
+          </div>
       </div>
     </div>
   </div>
