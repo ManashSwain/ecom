6408490fcb1a6b0e89e9042996f961454e8a659d
ManashSwain <manash0802@gmail.com>
2024-09-17 10:47:18 +0530

    added user schema

--- Models/Product.js
+++ Models/Product.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
     title : {type : String , required : true},
-    slug : {type : String , required : true},
+    slug : {type : String , required : true , unique : true},
     description : {type : String , required : true},
     image : {type : String , required : true},
     category : {type : String , required : true},
